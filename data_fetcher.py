aa74469283d08a69621e71231a0fb3e98f754a8e
hardman <hardman@debian>
2014-02-06 12:28:51 -0500

    First commit

---
+++ data_fetcher.py
@@ -0,0 +1,60 @@
+import string
+from web_client import WebClient
+
+__author__ = 'hardman'
+
+
+class FetcherBase():
+    def __init__(self, web_client, worker):
+        self.stop = False
+        self.worker = worker
+        self.web_client = web_client
+
+    def get_url(self):
+        raise NotImplemented
+
+    def process(self):
+        while not self.stop:
+            url = self.get_url()
+            content = ""
+            try:
+                content = self.web_client.get(url)
+            except Exception as e:
+                print str(e)
+            try:
+                self.worker(url, content)
+            except Exception as e:
+                print str(e)
+
+
+class RangeFetcher(FetcherBase):
+    def __init__(self, web_client, worker, template, start, end):
+        FetcherBase.__init__(self, web_client, worker)
+        self.template = template
+        self.start = start
+        self.end = end
+
+    def get_url(self):
+        ret_val = self.template % self.start
+        self.start += 1
+        if self.start > self.end:
+            self.stop = True
+        return ret_val
+
+
+class UrlFileFetcher(FetcherBase):
+    def __init__(self, filename, web_client, worker):
+        FetcherBase.__init__(self,web_client, worker)
+        f = open(filename, "r")
+        self.lines = f.readlines()
+        f.close()
+        self.start = 0
+        self.end = len(self.lines)
+
+    def get_url(self):
+        url = self.lines[self.start].strip()
+        self.start += 1
+        if self.start >= self.end:
+            self.stop = True
+        return url
+
---
+++ web_client.py
@@ -0,0 +1,24 @@
+import urllib2
+
+__author__ = 'hardman'
+
+
+class WebClient:
+    def __init__(self):
+        self.opener = urllib2.build_opener()
+        self.encoding = None
+        self.use_encoding = True
+        self.connection_timeout = None
+        self.opener.addheaders = [("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0"),
+                                  ("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")]
+
+    def get(self, url):
+        if self.encoding is None:
+            self.encoding = "utf-8"
+        if self.connection_timeout is None:
+            self.connection_timeout = 2
+        response = self.opener.open(url, None, self.connection_timeout)
+        data = response.read()
+        if self.use_encoding:
+            return data.decode(self.encoding)
+        return data
